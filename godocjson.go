8fa78375fb8a895850e4d11a338ef0923a5006f0
Christopher Swenson <chris@caswenson.com>
2015-05-29 18:30:28 -0700

    Flesh out function type switch

--- godocjson.go
+++ godocjson.go
@@ -8,6 +8,7 @@ import (
 	"go/parser"
 	"go/token"
 	"os"
+	"strings"
 )
 
 // Func represents a function declaration.
@@ -91,11 +92,52 @@ type FuncParam struct {
 	Name string `json:"name"`
 }
 
+func typeOf(x interface{}) string {
+	switch x := x.(type) {
+	case *ast.Ident:
+		return x.String()
+	case *ast.ArrayType:
+		return "[]" + typeOf(x.Elt)
+	case *ast.Field:
+		return x.Names[0].Name + " " + typeOf(x.Type)
+	case *ast.StructType:
+		fields := make([]string, x.Fields.NumFields())
+		for i, f := range x.Fields.List {
+			fields[i] = typeOf(f.Type)
+		}
+		return fmt.Sprintf("struct{%s}", strings.Join(fields, ","))
+	case *ast.InterfaceType:
+		methods := make([]string, x.Methods.NumFields())
+		for i, m := range x.Methods.List {
+			methods[i] = typeOf(m.Type)
+		}
+		return fmt.Sprintf("interface{%s}", strings.Join(methods, ","))
+	case *ast.SelectorExpr:
+		return typeOf(x.X) + "." + x.Sel.Name
+	case *ast.Ellipsis:
+		return "..." + typeOf(x.Elt)
+	case *ast.StarExpr:
+		return "*" + typeOf(x.X)
+	case *ast.FuncType:
+		params := make([]string, x.Params.NumFields())
+		for i, p := range x.Params.List {
+			params[i] = typeOf(p.Type)
+		}
+		results := make([]string, x.Results.NumFields())
+		for i, r := range x.Results.List {
+			results[i] = typeOf(r.Type)
+		}
+		return fmt.Sprintf("func(%s)%s", strings.Join(params, ","), strings.Join(results, ","))
+	default:
+		panic(fmt.Sprintf("Unknown type %+v", x))
+	}
+}
+
 func processFuncDecl(d *ast.FuncDecl, fun *Func) {
 	fun.Params = make([]FuncParam, d.Type.Params.NumFields())
 	for i, f := range d.Type.Params.List {
 		fun.Params[i] = FuncParam{
-			Type: "", // TODO: do a type switch on reflect.TypeOf(f.Type)
+			Type: typeOf(f.Type), // TODO: do a type switch on reflect.TypeOf(f.Type)
 			Name: f.Names[0].String(),
 		}
 	}
